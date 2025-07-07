4efdbe13dfa4ccdfeef5279e552c9e378b2b1c29
cmd-is-xka <2840931695@qq.com>
2025-07-07 14:45:37 +0800

    34

--- Gdkjmain/main.py
+++ Gdkjmain/main.py
@@ -3,9 +3,36 @@ import sys
 import os
 sys.path.append(os.getcwd())
 from writeini.wirte_ini import *
+import xml.etree.ElementTree as ET
+import json
+
 #最后运行地方.
 if __name__ == '__main__':
     writr_ini('Gdkj')
-    pytest.main(["gdkjtestpro/test_login.py::TestAllTestName", '-v', '-s','--alluredir=./result','--junitxml=junit/test-results-gdkj.xml'])
+    pytest.main(["gdkjtestpro/test_login.py::TestAllTestName", '-v', '-s','--junitxml=junit/test-results-gdkj.xml'])
     # pytest.main(["gdkjtestpro", '-v', '-s','--junitxml=junit/test-results-gdkj.xml','-k','not test_login'])
 
+    # 读取 XML 文件
+    tree = ET.parse('junit/test-results-gdkj.xml')
+    root = tree.getroot()
+
+    # 提取 testsuite 元素
+    testsuite = root.find('testsuite')
+
+    # 获取所需的属性值
+    errors = testsuite.get('errors')
+    failures = testsuite.get('failures')
+    skipped = testsuite.get('skipped')
+    tests = testsuite.get('tests')
+
+    content = {
+        '测试结果':'详情如下',
+        '错误':errors,
+        '失败':failures,
+        '跳过':skipped,
+        '总数':tests
+    }
+    
+    result_file_name = "result.json"
+    with open('Gdkjmain/%s'%(result_file_name), 'w', encoding='utf-8') as f:
+        json.dump(content, f, ensure_ascii=False, indent=4)
--- notify.py
+++ notify.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def send_dingtalk_msg(content):
     webhook = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=fd9662b6-1aa1-4944-87ff-03f33012dd39'
     headers = {"Content-Type": "application/json"}
@@ -11,11 +12,12 @@ def send_dingtalk_msg(content):
     print("DingTalk response:", r.text)
 
 
-with open('junit/test-results-gdkj.xml', 'r', encoding='utf-8') as f:
+with open('Gdkjmain/result.json', 'r', encoding='utf-8') as f:
     content = f.read()
 # 你可以根据 test_api.py 的运行结果生成内容，比如：
 # 示例直接发静态消息
 send_dingtalk_msg(content)
+
 
 
 
